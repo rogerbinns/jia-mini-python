f5157e0aba21cd6e12bb5de1c6bb7c833493c978
Roger Binns <rogerb@rogerbinns.com>
2013-05-26 03:40:18 -0700

    double check listcomp name scoping

--- test/listcomp.py
+++ test/listcomp.py
@@ -13,3 +13,8 @@ for i in range(1500):
     assert [[x,y,z] for x in range(3) for y in range(3) if x==2 for z in range(3)] == [[2, 0, 0], [2, 0, 1], [2, 0, 2], [2, 1, 0], [2, 1, 1], [2, 1, 2], [2, 2, 0], [2, 2, 1], [2, 2, 2]]
 
     assert [[x for x in range(2)] for x in range(3)] == [[0, 1], [0, 1], [0, 1]]
+
+# verify name scoping
+x=[[x for x in range(2)] for x in range(3)]
+assert x==[[0, 1], [0, 1], [0, 1]]
+assert x==[[x for x in range(2)] for x in range(3)]
